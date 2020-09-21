0afc62b54a01e1b9c006db6d2ef16d84497bf2ff
Vitor <vitor_cimitan_9@hotmail.com>
2020-09-20 23:44:53 -0300

    energia retornando os patches

--- redeNeural/energia.py
+++ redeNeural/energia.py
@@ -1,22 +1,20 @@
 from PIL import Image
 from numpy import array
 import numpy as np
-from sklearn.preprocessing import StandardScaler
 import dill
 
 def calculaEnergia(img):
+    patches = []
     soma = np.sum(np.square(img), axis=0)
-    ss = StandardScaler()
-    ss.fit(soma)
-    dill.dump(ss, open('ss.dill','wb'))
-    soma = ss.transform(soma)
-    print(soma.shape)
-            
+    soma[:] = [x / soma[np.argmax(soma)] for x in soma]
+    for i in range(0,soma.shape[0]):
+        if (soma[i] > 0.5):
+            patches.append(int(i/26))
+    return set(patches)
+    
 
 imagem = Image.open("025f3fd2.png")
 arr = array(imagem)
-print(arr.shape)
 arr = arr[:,:,:3]
 arr = np.mean(arr, axis=-1, keepdims=True)
-print(arr.shape)
 calculaEnergia(arr)
